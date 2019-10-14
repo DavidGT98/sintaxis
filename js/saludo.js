c397361c3bb528f97388fcb56cd26860a01a224b
david <david98gt@gmail.com>
2019-10-14 17:34:10 +0200

    primer commit

---
+++ js/saludo.js
@@ -0,0 +1,11 @@
+alert("Hola mundo");
+/* if(confirm("Mensaje de confirmacion")){
+  prompt("¿Cúal es tu nombre?");
+}
+ */
+document.write("prueba del metodo write");
+console.log("mensaje de información");
+
+console.warn("This is a warning!");
+
+console.error("This is an error!");
