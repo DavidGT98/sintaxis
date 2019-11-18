bcf90dda5412657ae320c052f46c274a14541ba2
DavidGT98 <david98gt@gmail.com>
2019-11-18 17:46:31 +0100

    Carrito-V2

--- js/carrito.js
+++ js/carrito.js
@@ -3,6 +3,7 @@
 
 class Carrito {
     constructor() {
+        this.listaArticulos = [];
         this.unidades = 0;
         this.precio = 0;
     }
@@ -10,12 +11,30 @@ class Carrito {
 
 var MiCarrito = new Carrito;
 
-function Añadir(precio) {
-    MiCarrito.unidades++;
-    MiCarrito.precio = parseInt(MiCarrito.precio) + parseInt(precio);
-    document.getElementById("resultado").innerHTML = "Unidades: " + MiCarrito.unidades + "<br>" +  "Precio: " +  MiCarrito.precio;
+class Articulo {
+    constructor() {
+        this.nombre = "";
+        this.precio = "";
+    }
 }
 
+function añadir(nombre, precio) {
+    var MiArticulo = new Articulo;
 
+    MiArticulo.nombre = nombre;
+    MiArticulo.precio = precio;
+    MiCarrito.listaArticulos.push(MiArticulo);
+    MiCarrito.unidades++;
+    MiCarrito.precio = parseInt(MiCarrito.precio) + parseInt(precio);
+    document.getElementById("resultado").innerHTML = "<h2>Carrito:</h2>" + "Unidades: " + MiCarrito.unidades + "<br>" + "Precio: " + MiCarrito.precio;
+}
+
+function mostrar() {
+    var cadena = "<h2> Productos: </h2>";
+    MiCarrito.listaArticulos.forEach(articulo => {
+        cadena += "Nombre:" + articulo.nombre + "<br>" + "Precio:" + articulo.precio + "<br>";
+    });
+    document.getElementById("resultado").innerHTML = cadena;
+}
 
 
